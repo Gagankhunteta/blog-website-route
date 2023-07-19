3edf5f49f6b93aa181469e872483598b4dc237ec
Gagan Khunteta <44227514+Gagankhunteta@users.noreply.github.com>
2023-07-19 15:25:14 +0530

    project

---
+++ src/pages/About.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className="mb-20">
+
+    <h1 className="sm:text-4xl text-2xl font-bold my-6 text=gray-900"> About</h1>
+    <p className="mx-auto leading-relaxed text-base mb-4">
+        Laraoraoa Laraoraoa Laraoraoa Laraoraoa Laraoraoa Laraoraoa Laraoraoa Laraoraoa Laraoraoa 
+    Laraoraoa Laraoraoa Laraoraoa 
+    Laraoraoa 
+    Laraoraoa 
+    v
+    Laraoraoa   </p>
+    <p className="mx-auto leading-relaxed text-base mb-4">Laraoraoa Laraoraoa Laraoraoa Laraoraoa Laraoraoa Laraoraoa Laraoraoa Laraoraoa Laraoraoa 
+    Laraoraoa Laraoraoa Laraoraoa 
+    Laraoraoa 
+    Laraoraoa 
+    v
+    Laraoraoa   </p>
+    <p className="mx-auto leading-relaxed text-base mb-4">Laraoraoa Laraoraoa Laraoraoa Laraoraoa Laraoraoa Laraoraoa Laraoraoa Laraoraoa Laraoraoa 
+    Laraoraoa Laraoraoa Laraoraoa 
+    Laraoraoa 
+    Laraoraoa 
+    v
+    Laraoraoa   </p>
+
+
+</div>
+  )
+}
---
+++ src/pages/article-content.js
@@ -0,0 +1,27 @@
+const articles = [
+    {
+      name: "learn-react",
+      title: "The Fastest Way to Learn React",
+      thumbnail: "/images/blog1.jpeg",
+      content: [
+        `Welcome! Today we're g hashdha jasdjlasnkdlasndlansnd g hashdha jasdjlasnkdlasndlansnd g hashdha jasdjlasnkdlasndlansnd g hashdha jasdjlasnkdlasndlansnd g hashdha jasdjlasnkdlasndlansnd g hashdha jasdjlasnkdlasndlansnd g hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndvv v v v g hashdha jasdjlasnkdlasndlansnd vg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansnd`,
+      ],
+    },
+    {
+      name: "learn-node",
+      title: "How to Build a Node Server in 10 Minutes",
+      thumbnail: "/images/blog2.jpeg",
+      content: [
+        `In this article, g hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansnd`,
+      ],
+    },
+    {
+      name: "my-thoughts-on-learning-react",
+      title: "My Thoughts on Learning ReactJS",
+      thumbnail: "/images/blog3.jpeg",
+      content: [
+        `Today  g hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansndg hashdha jasdjlasnkdlasndlansnd`,
+      ],
+    },
+  ];
+  export default articles;
